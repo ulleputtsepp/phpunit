fe482ab4b0b001247827cc2bd090908a5e998992
ulleputtsepp <ulle.puttsepp@khk.ee>
2019-01-19 01:17:38 +0200

    2_Extend a unit test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -9,15 +9,29 @@ namespace TDD\Test;
 require 'vendor\autoload.php';
 
 use PHPUnit\Framework\TestCase;
-use TDD\Receipt;
+use TDD\Receipt; //kasuta namespece-s olevat klassi
 
 class ReceiptTest extends TestCase {
+    //new instance, enne iga meetodi väljakutsumist
+    public function setUp() {
+        $this->Receipt = new Receipt();
+    }
+    //kustutab loodud objekti, unset the instance,
+    //runs from one to the next
+    public function tearDown() {
+        unset($this->Receipt);
+    }
+    //refactor testTotal
     public function testTotal() {
-        $Receipt = new receipt();
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
+        //võrdleb
         $this->assertEquals(
-            14,
-            $Receipt->total([0,2,5,8]),
+            //oodatav väärtus
+            15,
+            $output,
             'When summing the total should equal 15'
         );
     }
 }
+

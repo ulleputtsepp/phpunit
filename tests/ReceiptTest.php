c9e8980bf1b58d30f75c37063f7cc27882db8f96
ulleputtsepp <ulle.puttsepp@khk.ee>
2019-01-21 20:02:53 +0200

    7_Doubles: Test mock

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -48,6 +48,32 @@ class ReceiptTest extends TestCase {
         );
     }
 
+    //method for building a mock instance of the Receipt class
+    public function testPostTaxTotal() {
+        //for test mock use predefined values for the items, tax, coupon
+        $items = [1,2,5,8];
+        $tax = 0.20;
+        $coupon = null;
+        //build a mock PHPUnit
+        //and add namespace name of the class we want to build
+        //doubles need to be saved to a local variable, set it to local $Receipt variable
+        $Receipt = $this->getMockBuilder('TDD\Receipt')
+            //the call setMethods takes an array of methods (as parameter) to respond to
+            ->setMethods(['tax', 'total'])
+            ->getMock();
+        //methods only called once, correct inputs
+        $Receipt->expects($this->once())
+            ->method('total')
+            ->with($items, $coupon)
+            ->will($this->returnValue(10.00));
+        $Receipt->expects($this->once())
+            ->method('tax')
+            ->with(10.00, $tax)
+            ->will($this->returnValue(1.00));
+        $result = $Receipt->postTaxTotal([1,2,5,8], 0.20, null);
+        $this->assertEquals(11.00, $result);
+    }
+
     //tax calculation test method
     public function testTax() {
         $inputAmount = 10.00;

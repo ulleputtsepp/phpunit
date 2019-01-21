ae51f200aea8e749537a7a9e621c4d6a6ccfa480
ulleputtsepp <ulle.puttsepp@khk.ee>
2019-01-21 19:24:25 +0200

    7_Doubles: Test mock

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -47,26 +47,6 @@ class ReceiptTest extends TestCase {
             'When summing the total should equal 12'
         );
     }
-    //method for building a mock instance of the Receipt class
-    public function testPostTaxTotal() {
-        //build a mock PHPUnit
-        //and add namespace name of the class we want to build
-        //doubles need to be saved to a local variable, set it to local $Receipt variable
-        $Receipt = $this->getMockBuilder('TDD\Receipt')
-            //we ignore some of the specific features of a mock test calss and build a stub
-            //the call setMethods takes an array of methods (as parameter) to respond to
-            ->setMethods(['tax', 'total'])
-            ->getMock();
-        //define the 'total' and 'tax' methods our stub will perform
-        $Receipt->method('total')
-            //will method will specify what the stub method will do
-            ->will($this->returnValue(10.00));
-        $Receipt->method('tax')
-            ->will($this->returnValue(1.00));
-        //call method and assert expected result - array of items, tax percentage and coupon; the values won't actually make sense given what our stub is returning
-        $result = $Receipt->postTaxTotal([1,2,5,8], 0.20, null);
-        $this->assertEquals(11.00, $result);
-}
 
     //tax calculation test method
     public function testTax() {
